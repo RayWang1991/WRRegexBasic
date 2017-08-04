a59f0857b1d3e7a8ec17f35edafbfd83f71e105b
ray wang <wangrui@bongmi.com>
2017-08-04 11:10:43 +0800

    test FABuilder eplision NFA basic functions

--- WRRegexBasics/main.m
+++ WRRegexBasics/main.m
@@ -7,6 +7,9 @@
 #import <Foundation/Foundation.h>
 #import "WRParsingBasicLib.h"
 #import "WRRegexLib.h"
+#import "WRREState.h"
+
+void testState();
 
 void testLanguage();
 
@@ -16,9 +19,11 @@ void examToken(WRTerminal *terminal, WRRegexTokenType type, NSArray <WRCharRange
 void testMapper();
 
 void testCharRange();
-void rangeContentExam(WRCharRange *range, WRChar start, WRChar end);
+void examRangeContent(WRCharRange *range, WRChar start, WRChar end);
 
 void testCharRangeSetAlgorithm();
+
+void testFABuilder();
 
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
@@ -26,9 +31,20 @@ int main(int argc, const char *argv[]) {
 //    testCharRangeSetAlgorithm();
 //    testLanguage();
 //    testScanner();
-    testMapper();
+//    testState ();
+//    testMapper();
+    testFABuilder();
   }
   return 0;
+}
+
+void testState() {
+  WRREState *state1 = [[WRREState alloc] initWithStateId:0];
+  WRREState *state2 = [[WRREState alloc] initWithStateId:0];
+  NSMutableSet <WRREState *>* set = [NSMutableSet set];
+  [set addObject:state1];
+  [set addObject:state2];
+  assert(set.count == 1);
 }
 
 void testLanguage() {
@@ -62,7 +78,7 @@ void testScanner() {
   // basic
   [scanner setNumOfEof:0];
   scanner.inputStr = @"ab";
-  [scanner reset];
+  [scanner resetTokenIndex];
   [scanner startScan];
   [scanner scanToEnd];
   assert(scanner.tokens.count == 2);
@@ -72,21 +88,21 @@ void testScanner() {
   // escape
   [scanner setNumOfEof:0];
   scanner.inputStr = @"\\a\\b\\n\\s\\w\\d\\\\";
-  [scanner reset];
+  [scanner resetTokenIndex];
   [scanner startScan];
   [scanner scanToEnd];
   assert(scanner.tokens.count == 7);
   examToken(scanner.tokens[0],
             tokenTypeChar,
-            @[[[WRCharRange alloc] initWithStart:'a'
+            @[[[WRCharRange alloc]        initWithStart:'a'
                                           andEnd:'z']]);
   examToken(scanner.tokens[1],
             tokenTypeChar,
-            @[[[WRCharRange alloc] initWithStart:'b'
+            @[[[WRCharRange alloc]        initWithStart:'b'
                                           andEnd:'b']]);
   examToken(scanner.tokens[2],
             tokenTypeChar,
-            @[[[WRCharRange alloc] initWithStart:'\n'
+            @[[[WRCharRange alloc]        initWithStart:'\n'
                                           andEnd:'\n']]);
   examToken(scanner.tokens[3], tokenTypeChar, @[[[WRCharRange alloc] initWithChar:' '],
     [[WRCharRange alloc] initWithChar:'\t'],
@@ -94,23 +110,23 @@ void testScanner() {
     [[WRCharRange alloc] initWithChar:'\r']]);
   examToken(scanner.tokens[4],
             tokenTypeChar,
-            @[[[WRCharRange alloc] initWithStart:'0'
+            @[[[WRCharRange alloc]        initWithStart:'0'
                                           andEnd:'9'],
-              [[WRCharRange alloc] initWithStart:'a'
+              [[WRCharRange alloc]        initWithStart:'a'
                                           andEnd:'z'],
-              [[WRCharRange alloc] initWithStart:'A'
+              [[WRCharRange alloc]        initWithStart:'A'
                                           andEnd:'Z'],
             ]);
   examToken(scanner.tokens[5],
             tokenTypeChar,
-            @[[[WRCharRange alloc] initWithStart:'0'
+            @[[[WRCharRange alloc]        initWithStart:'0'
                                           andEnd:'9']]);
   examToken(scanner.tokens[6], tokenTypeChar, @[[[WRCharRange alloc] initWithChar:'\\']]);
 
   // char range test 1
   [scanner setNumOfEof:0];
   scanner.inputStr = @"[-ac-a-]";
-  [scanner reset];
+  [scanner resetTokenIndex];
   [scanner startScan];
   [scanner scanToEnd];
   assert(scanner.tokens.count == 1);
@@ -125,33 +141,32 @@ void testScanner() {
   // char range test 2
   [scanner setNumOfEof:0];
   scanner.inputStr = @"[acb]";
-  [scanner reset];
+  [scanner resetTokenIndex];
   [scanner startScan];
   [scanner scanToEnd];
   assert(scanner.tokens.count == 1);
   examToken(scanner.tokens[0], tokenTypeChar,
             @[[[WRCharRange alloc] initWithChar:'a'],
               [[WRCharRange alloc] initWithChar:'c'],
-              [[WRCharRange alloc] initWithStart:'b'
+              [[WRCharRange alloc]        initWithStart:'b'
                                           andEnd:'b']
             ]);
 }
 
-void testMapper(){
+void testMapper() {
   WRRegexScanner *scanner = [[WRRegexScanner alloc] init];
   // basic
   [scanner setNumOfEof:0];
   scanner.inputStr = @"[a-df]h\\d";
-  [scanner reset];
+  [scanner resetTokenIndex];
   [scanner startScan];
   [scanner scanToEnd];
-  
+
   WRCharRangeNormalizeMapper *mapper =
     [[WRCharRangeNormalizeMapper alloc] initWithRanges:scanner.ranges];
-  for(WRCharTerminal *charTerminal in scanner.charTerminals){
+  for (WRCharTerminal *charTerminal in scanner.charTerminals) {
     charTerminal.rangeIndexes = [mapper decomposeRangeList:charTerminal.ranges];
-  }
-  ;
+  };
 }
 
 void examToken(WRTerminal *terminal, WRRegexTokenType type, NSArray <WRCharRange *> *rangeList) {
@@ -196,43 +211,85 @@ void testCharRangeSetAlgorithm() {
   WRCharRange *range2 = [[WRCharRange alloc] initWithStart:3
                                                     andEnd:4];
   WRCharRangeNormalizeMapper *al = [[WRCharRangeNormalizeMapper alloc] initWithRanges:@[range1, range2]];
-  rangeContentExam(al.normalizedRanges[0], 0, 2);
-  rangeContentExam(al.normalizedRanges[1], 3, 4);
+  examRangeContent(al.normalizedRanges[0], 0, 2);
+  examRangeContent(al.normalizedRanges[1], 3, 4);
 
   WRCharRange *range3 = [[WRCharRange alloc] initWithStart:1
                                                     andEnd:7];
   WRCharRangeNormalizeMapper
     *al1 = [[WRCharRangeNormalizeMapper alloc] initWithRanges:@[range1, range2, range3]];
   assert(al1.normalizedRanges.count == 4);
-  rangeContentExam(al1.normalizedRanges[0], 0, 0);
-  rangeContentExam(al1.normalizedRanges[1], 1, 2);
-  rangeContentExam(al1.normalizedRanges[2], 3, 4);
-  rangeContentExam(al1.normalizedRanges[3], 5, 7);
+  examRangeContent(al1.normalizedRanges[0], 0, 0);
+  examRangeContent(al1.normalizedRanges[1], 1, 2);
+  examRangeContent(al1.normalizedRanges[2], 3, 4);
+  examRangeContent(al1.normalizedRanges[3], 5, 7);
 
   WRCharRange *range4 = [[WRCharRange alloc] initWithStart:7
                                                     andEnd:7];
   WRCharRangeNormalizeMapper *al2 = [[WRCharRangeNormalizeMapper alloc] initWithRanges:@[range4, range3]];
   assert(al2.normalizedRanges.count == 2);
-  rangeContentExam(al2.normalizedRanges[0], 1, 6);
-  rangeContentExam(al2.normalizedRanges[1], 7, 7);
+  examRangeContent(al2.normalizedRanges[0], 1, 6);
+  examRangeContent(al2.normalizedRanges[1], 7, 7);
 
   WRCharRange *range5 = [[WRCharRange alloc] initWithStart:6
                                                     andEnd:10];
   WRCharRangeNormalizeMapper
     *al3 = [[WRCharRangeNormalizeMapper alloc] initWithRanges:@[range5, range2, range1]];
   assert(al3.normalizedRanges.count == 3);
-  rangeContentExam(al3.normalizedRanges[0], 0, 2);
-  rangeContentExam(al3.normalizedRanges[1], 3, 4);
-  rangeContentExam(al3.normalizedRanges[2], 6, 10);
+  examRangeContent(al3.normalizedRanges[0], 0, 2);
+  examRangeContent(al3.normalizedRanges[1], 3, 4);
+  examRangeContent(al3.normalizedRanges[2], 6, 10);
 
   WRCharRangeNormalizeMapper
     *al4 = [[WRCharRangeNormalizeMapper alloc] initWithRanges:@[range1, range5, range2]];
   assert(al4.normalizedRanges.count == 3);
-  rangeContentExam(al4.normalizedRanges[0], 0, 2);
-  rangeContentExam(al4.normalizedRanges[1], 3, 4);
-  rangeContentExam(al4.normalizedRanges[2], 6, 10);
+  examRangeContent(al4.normalizedRanges[0], 0, 2);
+  examRangeContent(al4.normalizedRanges[1], 3, 4);
+  examRangeContent(al4.normalizedRanges[2], 6, 10);
 }
 
-void rangeContentExam(WRCharRange *range, WRChar start, WRChar end) {
+void examRangeContent(WRCharRange *range, WRChar start, WRChar end) {
   assert(range.start == start && range.end == end);
+}
+
+void testFABuilder(){
+  
+  WRLR1Parser *parser = [[WRLR1Parser alloc] init];
+  WRLanguage *language = [[WRRegexLanguage alloc] init];
+  WRRegexScanner *scanner = [[WRRegexScanner alloc] init];
+  scanner.inputStr = @"a+";
+  parser.language = language;
+  parser.scanner = scanner;
+  [parser prepare];
+  [parser startParsing];
+  
+//  [parser constructSPPF];
+//  [parser constructParseTree];
+  
+  WRTreeHorizontalDashStylePrinter *hdPrinter = [[WRTreeHorizontalDashStylePrinter alloc] init];
+  WRTreeLispStylePrinter *lispPrinter = [[WRTreeLispStylePrinter alloc] init];
+  [parser.parseTree accept:hdPrinter];
+  [parser.parseTree accept:lispPrinter];
+  [hdPrinter print];
+  [lispPrinter print];
+  WRAST *ast = [language astNodeForToken:parser.parseTree];
+  [hdPrinter reset];
+  [lispPrinter reset];
+  [ast accept:hdPrinter];
+  [ast accept:lispPrinter];
+  [hdPrinter print];
+  [lispPrinter print];
+  
+  WRCharRangeNormalizeMapper *mapper = [[WRCharRangeNormalizeMapper alloc]initWithRanges:scanner.ranges];
+  
+  for (WRCharTerminal *charTerminal in scanner.charTerminals) {
+    charTerminal.rangeIndexes = [mapper decomposeRangeList:charTerminal.ranges];
+  };
+  
+  WRREFABuilder *builder = [[WRREFABuilder alloc]initWithCharRangeMapper:mapper
+                                                                     ast:ast];
+  WRREState *epsilonStart = builder.epsilonNFAStart;
+//  [builder epsilonNFA2NFA];
+  ;
+  
 }
