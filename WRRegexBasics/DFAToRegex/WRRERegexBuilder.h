a2d42246c5292e71ae7d8d25383923f4a4730993
ray wang <wangrui@bongmi.com>
2017-08-13 22:53:41 +0800

    delay

--- WRRegexBasics/DFAToRegex/WRRERegexBuilder.h
+++ WRRegexBasics/DFAToRegex/WRRERegexBuilder.h
@@ -5,5 +5,18 @@
  */
 
 #import <Foundation/Foundation.h>
+#import "WRREFABuilder.h"
+#import "WRRERegexCarrier.h"
+#import "WRREDFAState.h"
+#import "WRREState.h"
+#import "WRRETransition.h"
+#import "WRCharRange.h"
+#import "WRCharRangeNormalizeMapper.h"
 
-@interface WRRERegexBuilder : NSObject @end
+@interface WRRERegexBuilder : NSObject
+
+//- (instancetype)initWithDFAStart:(WRREDFAState *)start
+//                    allDFAStates:(NSArray<WRREDFAState *> *)allStates
+//                          mapper:(WRCharRangeNormalizeMapper *)mapper
+//                           table:(int **)table;
+@end
