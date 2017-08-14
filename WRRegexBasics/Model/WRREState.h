ec98555448b2d09e3872920678cab3d84039178a
ray wang <wangrui@bongmi.com>
2017-08-14 21:29:59 +0800

    update

--- WRRegexBasics/Model/WRREState.h
+++ WRRegexBasics/Model/WRREState.h
@@ -8,7 +8,7 @@
 @class WRRETransition;
 @interface WRREState : NSObject
 @property (nonatomic, assign, readwrite) NSInteger stateId; // -1 under construction, <= -2 use hash, >= 0 use id
-@property (nonatomic, assign, readwrite) NSUInteger finalId;
+@property (nonatomic, assign, readwrite) NSUInteger finalId; // 0 for normal, > 0 for accept, < 0 for error
 @property (nonatomic, strong, readwrite) NSMutableArray <WRRETransition *> *fromTransitionList;
 @property (nonatomic, strong, readwrite) NSMutableArray <WRRETransition *> *toTransitionList;
 - (instancetype)initWithStateId:(NSInteger)stateId;
